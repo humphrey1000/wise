c6e68d864cba9efd70acec49b9e3c043b05916e4
humphrey <humblesys09@outlook.com>
2023-05-24 10:18:20 +0300

    main branch

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+	char day = 's';
+	char* point = &day;
+	std::cout<<point;
+return 0;
+}
+
---
+++ matrix.js
@@ -0,0 +1,8 @@
+
+let math = []
+function record(time,activity){
+	math.push([time, activity])
+	console.log(math)
+}
+record(0,2)
+record(1.5,2)
